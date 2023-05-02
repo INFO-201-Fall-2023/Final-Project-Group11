3c96eb2970123b7ef15ae172edaeda06a2e96d76
eliz3w <129106126+eliz3w@users.noreply.github.com>
2023-05-02 09:44:09 -0700

    Add files via upload

---
+++ data_wrangling.r
@@ -0,0 +1,5 @@
+library(dplyr)
+library(stringr)
+
+risk_df <- read.csv("Behavioral_Risk_Factor_Data__Tobacco_Use__2011_to_present_.csv")
+service_df <- read.csv("Behavioral-Health-Services-Provided-to-the-Medicaid-and-CHIP-Population.csv")
