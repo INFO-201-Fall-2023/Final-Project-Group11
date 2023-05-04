4ee160e76c299ab4e48317584ae33c4e500ab71e
Brittney Oeur <oeur.brittney@gmail.com>
2023-05-03 23:21:37 -0700

    - Removed behaviorial CHIP dataset - Added Seasonally Adjusted dataset

--- data_wrangling.r
+++ data_wrangling.r
@@ -2,4 +2,4 @@ library(dplyr)
 library(stringr)
 
 risk_df <- read.csv("Behavioral_Risk_Factor_Data__Tobacco_Use__2011_to_present_.csv")
-service_df <- read.csv("Behavioral-Health-Services-Provided-to-the-Medicaid-and-CHIP-Population.csv")
+service_df <- read.csv("Seasonally_Adjusted_LAUS_Estimates.csv")
