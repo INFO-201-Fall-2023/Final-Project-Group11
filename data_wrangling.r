bef5686dce60ef4e6882d000b48eb51bff88f708
Brittney Oeur <oeur.brittney@gmail.com>
2023-05-03 23:26:24 -0700

    - Updated variable name

--- data_wrangling.r
+++ data_wrangling.r
@@ -2,4 +2,4 @@ library(dplyr)
 library(stringr)
 
 risk_df <- read.csv("Behavioral_Risk_Factor_Data__Tobacco_Use__2011_to_present_.csv")
-service_df <- read.csv("Seasonally_Adjusted_LAUS_Estimates.csv")
+seasonally_df <- read.csv("Seasonally_Adjusted_LAUS_Estimates.csv")
