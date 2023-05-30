3b1745da4b06fb568267c72b363d12e674311474
Brittney Oeur <oeur.brittney@gmail.com>
2023-05-29 19:36:40 -0700

    - Made edits to intro

--- app.r
+++ app.r
@@ -10,8 +10,45 @@ df <- read.csv("unifiedData.csv")
 
 # DEFINE WHATS ON DIFFERENT PAGES
 intro_pg <- tabPanel("Background",
-                     h2("Tobacco Use in Washington state"),
-                     p("include background info, goals, pictures maybe, etc")
+                     h1("Tobacco Use in Washington state"),
+                     h2("include background info, goals, pictures maybe, etc"),
+                     p("The story our group intends to tell is about the relationship between tobacco use, 
+                       employment status and/or education level, and mental health. Such as how the use of tobacco/smoking, 
+                       the status of someone’s employment and/or education level may have an impact on their mental health."),
+                     
+                     p("We believe this an interesting and compelling story because it touches on a topic that affects a large portion of the 
+                       population. In recent years, the awareness of mental health has risen tremendously, and advocating for people to seek 
+                       professional help has been more accepted by mainstream culture. In addition, smoking is a widespread habit that affects
+                       the lives of millions of people, and its impact on mental health is often overlooked. By exploring this topic, we can 
+                       shed light on the issues. Moreover, the impact of employment status and/or education level on smoking and mental health 
+                       can highlight the larger issue of socioeconomic inequality. Within the context of the ongoing pandemic, the relationship
+                       between smoking and mental health has become even more pertinent. Stress and anxiety levels have been on the rise, which
+                       has led people to turn to smoking as a coping mechanism. This can then lead to an increased need for mental health 
+                       support, especially for those in lower socioeconomic brackets who may have more limited resources."),
+                     
+                     p("Some of the data we plan to look at includes causes of smoking in young adults, and the rates of this 
+                       over time and what treatment options have been presented across the country. These pieces of data are important
+                       to look at because it can show what issues are causing young adults to engage in these harmful behaviors, 
+                       and is it because of the lack of mental health care that is causing this. Analyzing this data can help us understand 
+                       the correlation between mental health issues and tobacco use. Another question that can be asked is how the rates of
+                       young adults using these products changed over time. This is another important question to think about because it can 
+                       show what significant cultural and historical changes have affected the rate of mental health in young adults to turn
+                       to tobacco use. (Ex. COVID). Another question that is worth asking is how the different states responded to this crisis.
+                       This is important to look at, as it can give insight on what solutions are working to combat this issue and what areas 
+                       are experiencing this problem at unprecedented levels. With this information, it can give lawmakers a general overview 
+                       of this issue and make them aware of what solutions are working, by comparing the data from other states and seeing 
+                       what solutions have worked and what have not."),
+                     
+                     p("Overall, this story is relevant and timely, given the pandemic and the extensive impact it has had on mental health.
+                       It is also a topic that affects a large portion of the population and is therefore of interest to many communities.
+                       By exploring the relationship between smoking, employment status, education level, and mental health, we can better 
+                       understand the complex interplay between these factors."),
+                     
+                     h2("Datasets that was used:"),
+                     tags$ul(
+                       tags$li("Behavioral Risk Factor Data: Tobacco Use (2011 to present)"),
+                       tags$li("Seasonally Adjusted LAUS Estimates"),
+                     )
   
 )
   
